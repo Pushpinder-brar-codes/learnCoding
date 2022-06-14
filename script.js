b194266a13a62df2eac34fca1ad36838c1d3ee14
Pushpinder Brar <brarac1@gmail.com>
2022-06-14 11:18:20 +0530

    tribute added

--- script.js
+++ script.js
@@ -8,3 +8,30 @@ cards.forEach((item) => {
     location.href = "/resources.html";
   });
 });
+
+let body = document.querySelector(".body");
+let tribute_div = document.querySelector(".tribute");
+let tribute_img = document.querySelector(".tribute_img");
+// body.addEventListener('load',(e)=>{
+//    location.href = "/tribute.html";
+// })
+
+setTimeout(() => {
+  tribute_img.classList.add("show_tribute");
+  console.log("show noww");
+}, 1000);
+
+setTimeout(() => {
+  tribute_img.classList.add("hide_tribute");
+  console.log("show noww");
+}, 5000);
+
+setTimeout(() => {
+  tribute_div.classList.add("hide_tribute");
+  console.log("show noww");
+  body.classList.remove("overflow_hidden");
+}, 7000);
+
+setTimeout(() => {
+  tribute_div.classList.add("display_none");
+}, 9000);
